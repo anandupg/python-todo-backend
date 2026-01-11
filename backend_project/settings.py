d7008fc00f50174226f11034dd8cec77f25bc451
anandupg <anandupganesh2026@mca.ajce.in>
2026-01-11 18:53:48 +0530

    enable CORS allow all for production debugging

--- backend_project/settings.py
+++ backend_project/settings.py
@@ -121,9 +121,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5173",
+#     "http://127.0.0.1:5173",
+# ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
